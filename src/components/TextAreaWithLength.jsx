8bf735e0193927de59753bfb3d4da40944c1b6d0
pconca <pconca@idealista.com>
2024-01-08 13:42:39 +0100

    * main added selected text

--- src/components/TextAreaWithLength.jsx
+++ src/components/TextAreaWithLength.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import '../styles/App.css';
 
 const TextAreaWithLength = () => {
   const [text, setText] = useState('');
+  const [selectedTextLenght, setSelectedTextLenght] = useState(0);
+  const textareaRef = useRef(null);
 
   const handleTextChange = (event) => {
     const newText = event.target.value;
-    console.log('newText: ' + newText);
     setText(newText);
   };
 
@@ -15,15 +16,31 @@ const TextAreaWithLength = () => {
     return text.split(/\.|\n+|\.\n+/).length;
   };
 
+  const countSelectedText = (event) => {
+    if (textareaRef.current === null) return;
+    var selectionStart = textareaRef.current.selectionStart;
+    var selectionStart = textareaRef.current.selectionStart;
+    var selectionEnd = textareaRef.current.selectionEnd;
+    if (selectionStart === undefined || selectionEnd === undefined) {
+      setSelectedTextLenght(0);
+      return;
+    }
+    var newLength = textareaRef.current.value.substring(selectionStart, selectionEnd).length;
+    setSelectedTextLenght(newLength);
+  }
+
   return (
     <div className="area-block">
       <textarea
+        ref={textareaRef}
         value={text}
         onChange={handleTextChange}
         placeholder="Type something..."
+        onMouseUp={countSelectedText}
       />
-      <p className="area-data">Length: {text.length}</p>
-      <p className="area-data">Sentences: {calculateSencences(text)}</p>
+      <div className="area-data">Length: {text.length}</div>
+      <div className="area-data">Sentences: {calculateSencences(text)}</div>
+      <div className="area-data">Selected text: {selectedTextLenght}</div>
     </div>
   );
 };
