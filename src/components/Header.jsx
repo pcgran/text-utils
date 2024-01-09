20c8b06261a898b478ad182b73a350ffe81a8468
pconca <pconca@idealista.com>
2024-01-09 18:07:12 +0100

    * main added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../styles/App.css";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="site-title">TextUtils</div>
+      <div className="header-separator">
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
