df63b0f1c4084ea6689446887ec15ea4d2124dff
pconca <pconca@idealista.com>
2024-01-24 17:08:02 +0100

    * main added copy button

--- src/components/DataContainer.jsx
+++ src/components/DataContainer.jsx
@@ -1,16 +1,33 @@
 import "../styles/App.css";
-
-const calculateSencences = (text) => {
-  if (text.length === 0) return 0;
-  return text.split(/\.|\n+|\.\n+/).length;
-};
+import { useState, useEffect } from "react";
 
 const DataContainer = ({ text }) => {
+
+  const [clipboard, setClipboard] = useState();
+
+  useEffect(() => {
+    setClipboard(navigator.clipboard);
+  }, []);
+
+  const calculateSencences = (text) => {
+    if (text.length === 0) return 0;
+    return text.split(/\.|\n+|\.\n+/).length;
+  };
+  
+  const copyText = () => {
+    clipboard.writeText(text);
+  }
+
   return (
-    <div className="data-container">
-      <div className="area-data"><span className="data-span">{text.length}</span> chars,</div>
-      <div className="area-data"><span className="data-span">{calculateSencences(text)}</span> sentences</div>
-    </div>
+    <section className="data-container">
+      <section id = "data-text">
+        <div className="area-data"><span className="data-span">{text.length}</span> chars,</div>
+        <div className="area-data"><span className="data-span">{calculateSencences(text)}</span> sentences</div>
+      </section>
+      <button id="copy-text" className="dark" onClick={copyText}>
+        Copy
+      </button>
+    </section>
   );
 };
 
